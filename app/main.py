98db79f79360d340733c4e97ef4238fdae7fa3c6
Johan Rangel <johan.apr99@gmail.com>
2025-12-26 12:32:30 +0000

    review car wash

--- app/main.py
+++ app/main.py
@@ -17,16 +17,17 @@ class CarWashStation:
         for car in list_of_cars:
             if car.clean_mark < self.clean_power:
                 income = self.calculate_washing_price(car)
-                car.clean_mark = self.wash_single_car()
+                self.wash_single_car()
                 cash_received += income
         return cash_received
         
     
     def calculate_washing_price(self, car):
-        return round(car.comfort_class * self.clean_power - car.clean_mark * self.average_rating / self.distance_from_city_center, 1)
+        differences_value = (self.clean_power - car.clean_mark)
+        return round((car.comfort_class * differences_value * self.average_rating) / self.distance_from_city_center, 1)
 
-    def wash_single_car(self):
-        return self.clean_power
+    def wash_single_car(self, car):
+        car.clean_mark = self.clean_power
     
     def rate_service(self, new_rating):
         self.average_rating = round((self.average_rating * self.count_of_ratings + new_rating) / (self.count_of_ratings + 1), 1)
