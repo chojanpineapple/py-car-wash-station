e2d513803b1a27c30306a8901b62415819dfe36e
Johan Rangel <johan.apr99@gmail.com>
2025-12-26 12:27:56 +0000

    car washing chojan

--- app/main.py
+++ app/main.py
@@ -1,8 +1,33 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float, count_of_ratings: int):
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+    
+    def serve_cars(self, list_of_cars: list):
+        cash_received = 0
+        for car in list_of_cars:
+            if car.clean_mark < self.clean_power:
+                income = self.calculate_washing_price(car)
+                car.clean_mark = self.wash_single_car()
+                cash_received += income
+        return cash_received
+        
+    
+    def calculate_washing_price(self, car):
+        return round(car.comfort_class * self.clean_power - car.clean_mark * self.average_rating / self.distance_from_city_center, 1)
+
+    def wash_single_car(self):
+        return self.clean_power
+    
+    def rate_service(self, new_rating):
+        self.average_rating = round((self.average_rating * self.count_of_ratings + new_rating) / (self.count_of_ratings + 1), 1)
+        self.count_of_ratings += 1
